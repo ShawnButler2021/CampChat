1cad3438b02bba37f2bd11129c72fa29b3bd8069
Shawn Butler <117597766+ShawnButler2021@users.noreply.github.com>
2024-01-12 16:53:28 -0600

    Add files via upload

--- backend.py
+++ backend.py
@@ -1,11 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 web_app = Flask(__name__)
 
 
-@web_app.route("/")
+@web_app.route("/", methods=["POST", "GET"])
 def index():
-  return render_template('index.html')
+  if request.method == "GET":
+    return render_template('index.html')
+
+  else:
+    if request.form["ip-address"] != None:
+      return redirect(for_url("room.html"))
+    return redirect(for_url("create_room.html"))
+
+
+@web_app.route("/create_room")
+def create_room():
+  return render_template('create_room.html')
+
+@web_app.route("/room")
+def room():
+  return render_template('room.html')
+
+
 
 
 
