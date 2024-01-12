9ae960efcf486e1f352d848f3d937103a497dbe5
Shawn Butler <117597766+ShawnButler2021@users.noreply.github.com>
2024-01-12 12:36:23 -0600

    Add files via upload

--- backend.py
+++ backend.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 web_app = Flask(__name__)
 
 
 @web_app.route("/")
-def home_page():
-  return "<p> homepage </p>"
+def index():
+  return render_template('index.html')
+
+
+
+web_app.run(debug=True)
